83f77305a096f855cf2667064e96886a7e1d51b8
samisami <samisami1215@icloud.com>
2022-04-14 21:31:43 +0900

    ヘッダー、フッター

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -10,7 +10,25 @@
 
 <body>
     <header>
-
+        <div class="header_all">
+            <div class="header_buttons">
+                <h1>HOME</h1>
+                <ul>
+                    <li>検索</li>
+                    <li>カート</li>
+                    <li>ログイン</li>
+                </ul>
+            </div>
+            <div class="header_ul">
+                <ul>
+                    <li>新着</li>
+                    <li>年齢別</li>
+                    <li>販売</li>
+                    <li>特集</li>
+                    <li>レビュー</li>
+                </ul>
+            </div>
+        </div>
     </header>
 
     <main>
@@ -18,7 +36,13 @@
     </main>
 
     <footer>
-
+        <ul>
+            <li>新着</li>
+            <li>年齢別</li>
+            <li>販売</li>
+            <li>特集</li>
+            <li>レビュー</li>
+        </ul>
     </footer>
 </body>
 </html>
