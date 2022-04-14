25a64f79c0c741970c23167a82b746d53da7080e
samisami <samisami1215@icloud.com>
2022-04-14 16:53:43 +0900

    トップページのmodel,view,controllerを用意。
    masterは親、homeは継承できるように記述

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+
+{{-- 継承ビュー --}}
+@extends('master')
+
+{{-- タイトル --}}
+@section('title','絵本の世界へようこそ')
+
+{{--トップページ ( 共通部分:親ファイル → master.blade.php ) --}}
+@section('content')
+<div class="top_title_images">
+    <h1>絵本の世界へようこそ</h1>
+    <p>- Picture Library -</p>
+</div>
+
+
+@endsection
---
+++ resources/views/master.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/master.css') }}">
+    <title>@yield('title')</title>
+</head>
+
+<body>
+    <header>
+
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
