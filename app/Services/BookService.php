cd8aa42967262f08e06447273fd6a02cfc5307fa
samisami <samisami1215@icloud.com>
2022-04-19 03:55:57 +0900

    ビューをインスタンスで表示する（services.phpファイルを作成）

--- app/Services/BookService.php
+++ app/Services/BookService.php
@@ -10,6 +10,7 @@ use App\Models\Book;
  * Book関連の処理をまとめたクラス
  */
 class BookService {
+
     /**
      * target_ageによりBookの配列をグルーピングする。
      *
@@ -22,46 +23,18 @@ class BookService {
         // target_ageでグルーピング処理
         foreach($books as $book) {
             $target_age = $book->target_age;
-            // $grouped_booksが未定義（NULL）のときに初期化する
-            // if(isset($grouped_books[$target_age]) === false) {
-            //     $grouped_books[$target_age] = [];
-            // }
 
             // $grouped_booksが未定義（NULL）のときに初期化する
             // null合体代入演算子を利用したバージョン
             $grouped_books[$target_age] ??= [];
             $grouped_books[$target_age][] = $book;
 
-
         }
 
         return $grouped_books;
 
-
-        /*
-         * [
-         *   new Book('だるまさん',"かがり","0","/images/guru.jpg"),
-         *   new Book('だるまさん',"かがり","0","/images/guru.jpg"),
-         *   new Book('いろえんぴつ',"みさ","2","/images/kingyo.jp"),
-         * ]
-         *
-         * [0][
-         *   new Book('だるまさん',"かがり","0","/images/guru.jpg"),
-         *   new Book('だるまさん',"かがり","0","/images/guru.jpg"),         *
-         * ]
-         * [2][
-         *   new Book('いろえんぴつ',"みさ","2","/images/kingyo.jp"),
-         * ]
-         *
-         * $grouped_books[0] = [];
-         * $grouped_books[2] = [];
-         *
-         * $grouped_books[0][] = $book;
-         * $grouped_books[0][] = $book;
-         * $grouped_books[2][] = $book;
-         *
-         * $grouped_books[2][0]
-         */
-
     }
+
+    // トップページに特集ページを表示する
+
 }
