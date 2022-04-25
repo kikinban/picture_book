9aa1966c42dbde36b162eb682538c6e3f202a9f6
samisami <samisami1215@icloud.com>
2022-04-26 08:23:29 +0900

    トップページに年齢を表示させる為に、作成した関数を削除

--- app/Services/BookService.php
+++ app/Services/BookService.php
@@ -12,29 +12,11 @@ use App\Models\Book;
 class BookService {
 
     /**
-     * target_ageによりBookの配列をグルーピングする。
+     * 新着絵本を表示するfunction
      *
-     * @param Book[] $books Bookの配列
-     * @return Book[target_age][] $grouped_books target_ageによりグルーピングされたBookの配列
+     * @return void
      */
-    public function groupByTargetAge(array $books) {
-        $grouped_books = [];
 
-        // target_ageでグルーピング処理
-        foreach($books as $book) {
-            $target_age = $book->target_age;
-
-            // $grouped_booksが未定義（NULL）のときに初期化する
-            // null合体代入演算子を利用したバージョン
-            $grouped_books[$target_age] ??= [];
-            $grouped_books[$target_age][] = $book;
-
-        }
-
-        return $grouped_books;
-
-    }
-
-    // トップページに特集ページを表示する
 
 }
+
