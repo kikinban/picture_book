e7f169e025247c6aaf1a7f6997a1f6562a2639fe
samisami <samisami1215@icloud.com>
2022-04-26 08:24:48 +0900

    booksテーブルをmysqlにインサートする処理を追加
    以前は違うphpファイルに書いていたコード内容だが、再利用性を高める為にリファクタリング（修正）した

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,28 +4,71 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-// class Home extends Model
-class Book
+
+/**
+ * データベースに接続して処理できるモデル
+ * （Eloquent）を利用する
+ */
+class Book extends Model
 {
-    // use HasFactory;
-    public string $title;
-    public string $writer;
-    public int $target_age;
-    public string $image_url;
+    use HasFactory;
 
-    public function __construct(
-        string $title,
-        string $writer,
-        int $target_age,
-        string $image_url
-    ){
-        $this->title = $title;
-        $this->writer = $writer;
-        $this->target_age = $target_age;
-        $this->image_url = $image_url;
+    // 更新していいカラムのホワイトリスト
+    // fillable = 書き込めるという意味
+    protected $fillable = [
+        'id',
+        'book_title',
+        'book_writer',
+        'book_target_age',
+        'book_image_url',
+        'book_page_number',
+        'book_description',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * MySQLにインサートするfunction
+     */
+    public static function insertData(array $values) {
+
+
+        // $valuesの先頭行にはフィールド名が入っている
+        $fields = $values[0];
+
+        /**
+         *  データの更新とインサートを同時に出来るようにしたい
+         */
+
+        // mysqlのデータを一度全削除する
+        Book::truncate();
+
+        foreach ($values as $row_count => $row) {
+
+            // 先頭の行はフィールドの為、読み込まない
+            // つまり、ループから抜ける処理
+            if ($row_count === 0) {
+                continue;
+            }
+
+            // 空の配列を用意する
+            $insert_row = [];
+            // $fieldsはフィールド名（先頭の行）を取得している
+            // $rowのインデックスと$fieldsのインデックスは同じ構造
+            // その為、$rowのインデックスを$fieldsのインデックスに置き換える
+            // また$fieldはフィールド名の為、配列のカラム名として入力できるようにする
+            $row[0] = (int)$values[0];
+            foreach ($fields as $index => $field) {
+                $insert_value = $row[$index];
+                $insert_row[$field] = $insert_value;
+            }
+
+            // 全データがmysqlに保存される
+            Book::create($insert_row);
+        }
     }
 
 }
