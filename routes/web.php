8599e2cd950659720336c52cc48c2fb20ff69cd8
samisami <samisami1215@icloud.com>
2022-05-02 03:11:15 +0900

    スプレッドシート読み込みページとトップページのルーティング追加
    Laravel-Breezeを入れたことにより、消えてしまった為。

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
+
+use App\Http\Controllers\SpreadSheetController;
+use App\Http\Controllers\TopController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,12 +21,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// ユーザー関連
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
 
+// 管理者関連
 Route::prefix('admin')->name('admin.')->group(function(){
 
     Route::get('/dashboard', function () {
@@ -31,3 +37,14 @@ Route::prefix('admin')->name('admin.')->group(function(){
 
     require __DIR__.'/admin.php';
 });
+
+// トップページ
+Route::get('/top', [TopController::class, 'bookDetail']);
+
+//  スプレッドシートデータインサート画面
+Route::get('/store', [SpreadSheetController::class, 'store']);
+
+
+
+
+
