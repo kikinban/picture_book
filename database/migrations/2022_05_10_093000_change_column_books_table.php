1b4df7ce06a2bee6c319da35380ffe90f3f2fb17
misa_kohata <samisami1215sami@gmail.com>
2022-05-11 00:16:14 +0900

    カラム型の変更（絵本の画像リンク・・・アマゾンリンク）
    初めはstring型にしていたが、規定の文字数を超えてエラーになった。
    その為、texr型に変更した。エラーは解消。

---
+++ database/migrations/2022_05_10_093000_change_column_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            //　カラムの型変更
+            $table->text('book_image_url')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            //
+            $table->string('book_image_url')->change();
+        });
+    }
+};
